99a1a929811fa9b9608718c7b1b92304baf1de35
John Franklin III <jfrankl6@mail.ccsf.edu>
2016-09-01 05:43:26 -0700

    my first commit
    Has Map and User Location

---
+++ mapView/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  mapView
+//
+//  Created by John Franklin on 9/1/16.
+//  Copyright © 2016 John Franklin. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.locationManager.requestAlwaysAuthorization()// stack overflow help not needed
+        
+        self.locationManager.delegate = self
+    
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        self.locationManager.startUpdatingLocation()
+        
+        self.mapView.showsUserLocation = true
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations.last
+        
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+        self.mapView.setRegion(region, animated: true)
+        
+        self.locationManager.stopUpdatingLocation()
+    }
+
+}
+
